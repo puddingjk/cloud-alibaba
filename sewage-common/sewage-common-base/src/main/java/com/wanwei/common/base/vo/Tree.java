4f3d519b89750d743412995e17c776af2429fe8f
luohongyu <luohongyu907@qq.com>
2021-10-24 15:26:38 +0800

    修改

--- sewage-common/sewage-common-base/src/main/java/com/wanwei/common/base/vo/Tree.java
+++ sewage-common/sewage-common-base/src/main/java/com/wanwei/common/base/vo/Tree.java
@@ -106,8 +106,8 @@ public class Tree {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o){ return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
         Tree tree = (Tree) o;
         return Objects.equals(id, tree.id) &&
                 Objects.equals(label, tree.label) &&
